ae5eaad465cc5a1075bfc5b985f0447a3bf75681
unknown <brunocoria55@gmail.com>
2022-07-12 23:05:46 -0300

    app java/sql/postgres

--- src/main/java/ar/com/codoacodo/connection/AdministradorDeConexiones.java
+++ src/main/java/ar/com/codoacodo/connection/AdministradorDeConexiones.java
@@ -3,32 +3,37 @@ package ar.com.codoacodo.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class AdministradorDeConexiones { //ctrl + space
-	
-	public static Connection  getConnection() {
- 		String url = "jdbc:mysql://127.0.0.1:3306/codo-a-codo?serverTimeZone=UTC&userSSL=false";
+public class AdministradorDeConexiones {
+
+	public static Connection getConnection() {
+	/*	String url = "jdbc:mysql://127.0.0.1:3306/codo-a-codo?serverTimeZone=UTC&userSSL=false";
 		String username = "root";
 		String password = "root";
 		
-		String driverName = "com.mysql.cj.jdbc.Driver"; //mySQL | postgres}
+		String driverName = "com.mysql.cj.jdbc.Driver";// mysql|postrges */
+		
+		String url = System.getenv("DATASOURCE_URL");
+		String user = System.getenv("DATASOURCE_USERNAME");
+		String password = System.getenv("DATASOURCE_PASSWORD");
+		String driverName = System.getenv("DATASOURCE_DRIVER");
 		
 		// control errores
 		Connection con = null;
 		try {
-			Class.forName(driverName); //Carga en memoria el driver
-			con = DriverManager.getConnection(url,username,password);
+			Class.forName(driverName);// carga en memoria el driver
+			con = DriverManager.getConnection(url,user,password);
 		}catch(Exception e) {
-			e.printStackTrace();// Se que paso y porque fallo
+			e.printStackTrace();// se que paso o porque fallo.
 		}
-		return con; 
+		return con;
 	}
 	
 	public static void main(String[] args) {
-		Connection con = AdministradorDeConexiones.getConnection();
+		Connection con = AdministradorDeConexiones.getConnection();//f5
 		if(con != null) {
 			System.out.println("Conexion ok");
-		} else {
-			System.out.println("Conexion fail");
+		}else {
+			System.err.println("Conexion fail");
 		}
 	}
 }
